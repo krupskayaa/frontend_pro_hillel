21b0b4d624ea1767809ef36b2e2f0495b25a2aec
krupskayaa <alyona.krupskaya@gmail.com>
2022-05-06 23:53:57 +0300

    worked on hw-7

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log('alyona')
+// Задача 1
+
+function toSum(array) {
+    if (!array.length) {
+      return 0;
+    } else {
+      return array[0] + toSum(array.slice(1));
+    }
+}
+
+console.log(toSum([2, 3, 4, 5, 6, 7]));
+
+// Задача 2
+
+function sum(start, end, step){
+    if(start > end){
+        return 0;
+    } else {
+        return start + sum(start + step, end, step)
+    }
+}
+console.log(sum(1, 5, 1));
