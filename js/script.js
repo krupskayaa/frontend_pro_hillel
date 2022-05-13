8fddf42967efaa00348f90a0aff4170cefeb0354
krupskayaa <alyona.krupskaya@gmail.com>
2022-05-13 20:25:22 +0300

    worked on hw-9

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('alyona')
+var enter = prompt('Введите время в формате (10:20, чч:мм), когда Вас разбудить');
+var timeArray = enter.split(':');
+var alarm = setInterval (function () {
+    var date = new Date();
+    var time = date.toLocaleTimeString()
+    document.getElementById('block').innerHTML = time
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    if (timeArray[0] == hours && timeArray[1] == minutes) {
+        alert('Вставайте!');
+        clearInterval(alarm);
+    }
+}, 1000)
