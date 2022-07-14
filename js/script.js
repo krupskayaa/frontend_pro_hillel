aebe8460a141e2bb54c73d06a4308bdecf997be5
krupskayaa <alyona.krupskaya@gmail.com>
2022-07-14 11:22:44 +0300

    checked

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ const pagination = {
 
 const url = 'https://pokeapi.co/api/v2/';
 
-const createElem = (tag, append, src = null, text = null, clName, api = null) => {
+const createElem = (tag, append, src = null, text = null, clName) => {
     let elem = document.createElement(tag);
     if(src){
         elem.src = src;
@@ -21,80 +21,82 @@ const createElem = (tag, append, src = null, text = null, clName, api = null) =>
         elem.innerHTML = text;
     }
     elem.classList.add(clName);
-    if(api){
-        elem.addEventListener('click', () => {
-            fetch(api)
-                .then(res => res.json())
-                .then(res => {
-                    const { sprites, species, abilities, base_experience, height, weight, types } = res;
-                    const { front_default } = sprites;
-                    const { name } = species;
-                    info.innerHTML = '';
-                    createElem('h2', info, null, name, 'title');
-                    createElem('img', info, front_default, name, 'img-pokemon');
-                    createElem('h3', info, null, 'Abilities:', 'abilities-txt');
-                    abilities.forEach(({ ability }, index) => {
-                        createElem('p', info, null, `${index+1}) ${ability.name}`, 'abilities');
-                    });
-                    createElem('h3', info, null, `base experience: <span>${base_experience}</span>`, 'abilities-txt');
-                    createElem('h3', info, null, `height: <span>${height}</span>`, 'abilities-txt');
-                    createElem('h3', info, null, `weight: <span>${weight}</span>`, 'abilities-txt');
-                    createElem('h3', info, null, 'Types:', 'abilities-txt');
-                    types.forEach(({ type }, index) => {
-                        createElem('p', info, null, `${index+1}) ${type.name}`, 'abilities');
-                    })
-                    fetch(species.url)
-                        .then(result => result.json())
-                        .then(result => {
-                            const { evolution_chain } = result;
-                            const { url } = evolution_chain;
-                            btnShow = document.createElement('button');
-                            btnShow.innerHTML = 'show evolution';
-                            btnShow.classList.add('btn-evolution');
-                            btnShow.addEventListener('click', () => {
-                                fetch(url)
-                                    .then(res => res.json())
-                                    .then(res => {
-                                        const { chain } = res;
-                                        createElem('div', info, null, null, 'wrap-evolution');
-                                        wrapEvolution = document.querySelector('.wrap-evolution');
-                                        wrapEvolution.innerHTML = '';
-                                        let array = [];
-                                        const getEvolution = (chain) => {
-                                            if (chain.evolves_to.length) {
-                                                getEvolution(chain.evolves_to[0]);
-                                            }
-                                            return array.push(chain.species.name);
-                                        }
-                                        getEvolution(chain);
-                                        array.reverse().join(' => ')
-                                        createElem('p', wrapEvolution, null, array.reverse().join(' => '), 'abilities-txt');
-                                    });
-                            })
-                            info.append(btnShow);
-                        });
-                        createElem('button', info, null, 'add to favourites', 'btn-favourites');
-                        const btnFavourites = document.querySelector('.btn-favourites');
-                        btnFavourites.addEventListener('click', () => {
-                            const allPokemons = document.querySelectorAll('.pokemon');                    
-                            favourites.push(name);
-                            sessionStorage.setItem('favourites', JSON.stringify(favourites));
-                            allPokemons.forEach(el => {
-                                if(favourites.includes(el.textContent)) {
-                                    el.classList.add('selected-pokemon');
-                                }
-                            })
-                        })
-                })
-        })
-    }
     append.append(elem);
     return elem;
 }
 
+const apiProcess = (api) => {
+    fetch(api)
+        .then(res => res.json())
+        .then(res => {
+            const { sprites, species, abilities, base_experience, height, weight, types } = res;
+            const { front_default } = sprites;
+            const { name } = species;
+            info.innerHTML = '';
+            createElem('h2', info, null, name, 'title');
+            createElem('img', info, front_default, name, 'img-pokemon');
+            createElem('h3', info, null, 'Abilities:', 'abilities-txt');
+            abilities.forEach(({ ability }, index) => {
+                createElem('p', info, null, `${index+1}) ${ability.name}`, 'abilities');
+            });
+            createElem('h3', info, null, `base experience: <span>${base_experience}</span>`, 'abilities-txt');
+            createElem('h3', info, null, `height: <span>${height}</span>`, 'abilities-txt');
+            createElem('h3', info, null, `weight: <span>${weight}</span>`, 'abilities-txt');
+            createElem('h3', info, null, 'Types:', 'abilities-txt');
+            types.forEach(({ type }, index) => {
+                createElem('p', info, null, `${index+1}) ${type.name}`, 'abilities');
+            })
+            fetch(species.url)
+                .then(result => result.json())
+                .then(result => {
+                    const { evolution_chain } = result;
+                    const { url } = evolution_chain;
+                    createElem('button', info, null, 'show-evolution', 'btn-evolution');
+                    btnShow = document.querySelector('.btn-evolution');
+                    btnShow.addEventListener('click', () => {
+                        fetch(url)
+                            .then(res => res.json())
+                            .then(res => {
+                                const { chain } = res;
+                                createElem('div', info, null, null, 'wrap-evolution');
+                                wrapEvolution = document.querySelector('.wrap-evolution');
+                                wrapEvolution.innerHTML = '';
+                                let array = [];
+                                const getEvolution = (chain) => {
+                                    if (chain.evolves_to.length) {
+                                        getEvolution(chain.evolves_to[0]);
+                                    }
+                                    return array.push(chain.species.name);
+                                }
+                                getEvolution(chain);
+                                array.reverse().join(' => ')
+                                createElem('p', wrapEvolution, null, array.reverse().join(' => '), 'abilities-txt');
+                            });
+                    })
+                    info.append(btnShow);
+                });
+                createElem('button', info, null, 'add to favourites', 'btn-favourites');
+                const btnFavourites = document.querySelector('.btn-favourites');
+                btnFavourites.addEventListener('click', () => {
+                    const allPokemons = document.querySelectorAll('.pokemon');                    
+                    favourites.push(name);
+                    sessionStorage.setItem('favourites', JSON.stringify(favourites));
+                    allPokemons.forEach(el => {
+                        if(favourites.includes(el.textContent)) {
+                            el.classList.add('selected-pokemon');
+                        }
+                    })
+                })
+        })
+}
+
+const checkPokeClass = (name) => {
+    return favourites.includes(name) ?
+        'selected-pokemon' :
+        'pokemon';
+}
 
 createElem('div', document.body, null, null, 'wrapper-pokemons');
-
 createElem('div', document.body, null, null, 'wrapper-paginat');
 const wrapperPaginat = document.querySelector('.wrapper-paginat');
 
@@ -131,11 +133,9 @@ const apiRequest = () => {
                 })
             }
             results.forEach(el => {
-                const pokemonClass = favourites.includes(el.name) ?
-                    'selected-pokemon' :
-                    'pokemon';
-
-                createElem('button', wrapper, null, el.name, pokemonClass, el.url);
+                const pokemonClass = checkPokeClass(el.name);
+                const elem = createElem('button', wrapper, null, el.name, pokemonClass);
+                elem.addEventListener('click', () => apiProcess(el.url));
             })
         })
 }
@@ -160,50 +160,44 @@ const apiRegion = () => {
                                 .then(res =>{
                                     const { pokemon_entries } = res;
                                     pokemons = pokemon_entries;
-                                    wrapper.innerHTML = '';
-                                    pokemons.slice(0, pagination.limit-1).forEach(el => {
-                                        const { pokemon_species } = el;
-                                        const { name } = pokemon_species;
-                                        const pokemonClass = favourites.includes(name) ?
-                                            'selected-pokemon' :
-                                            'pokemon';
-
-                                        createElem('button', wrapper, null, name, pokemonClass, `${url}pokemon/${name}`);
-                                    });
-                                    wrapperPaginat.innerHTML = '';
-                                    const countPages = Math.floor(pokemons.length / pagination.limit);
-
-                                    createPag(countPages);
-                                    const btnPaginat = document.querySelectorAll('.btn-paginat');
-
-                                    btnPaginat.forEach((elem, index) => {
-                                        elem.setAttribute('data-id', index + 1);
-                                        elem.setAttribute('href', '#');
-                                        elem.addEventListener('click', function (e){
-                                            e.preventDefault();
-                                            wrapper.innerHTML = '';
-                                            const start = pagination.limit * (elem.getAttribute('data-id') - 1);
-                                            const end = pagination.limit * elem.getAttribute('data-id') - 1;
-                                            pokemons.slice(start, end).forEach(el => {
-                                                const { pokemon_species } = el;
-                                                const { name } = pokemon_species;
-                                                const pokemonClass = favourites.includes(name) ?
-                                                    'selected-pokemon' :
-                                                    'pokemon';
-
-                                                createElem('button', wrapper, null, name, pokemonClass, `${url}pokemon/${name}`);
-                                            });
-                                        })
-                                    })
+                                    paginatePokemons(0, pagination.limit-1, 'pokemon_species');
+                                    createPagination('pokemon_species');
                                 })
                             } else {
-                                wrapper.innerHTML = `<p>No pokemon of this region :(</p>`
-                                wrapperPaginat.innerHTML = ''
+                                wrapper.innerHTML = `<p>No pokemon of this region :(</p>`;
+                                wrapperPaginat.innerHTML = '';
                             }
                         });
                 })
             })
         })
+}
+
+const paginatePokemons = (start, end, property) => {
+    wrapper.innerHTML = '';
+    pokemons.slice(start, end).forEach(el => {
+        const name = el[property].name;
+        const pokemonClass = checkPokeClass(name);
+        const elem = createElem('button', wrapper, null, name, pokemonClass);
+        elem.addEventListener('click', () => apiProcess(`${url}pokemon/${name}`));
+    });
+}
+
+const createPagination = (name) => {
+    wrapperPaginat.innerHTML = '';
+    const countPages = Math.floor(pokemons.length / pagination.limit);
+    createPag(countPages);
+    const btnPaginat = document.querySelectorAll('.btn-paginat');
+    btnPaginat.forEach((elem, index) => {
+        elem.setAttribute('data-id', index + 1);
+        elem.setAttribute('href', '#');
+        elem.addEventListener('click', function (e){
+            e.preventDefault();
+            const start = pagination.limit * (elem.getAttribute('data-id') - 1);
+            const end = pagination.limit * elem.getAttribute('data-id') - 1;
+            paginatePokemons(start, end, name);
+        })
+    })
 }
 
 const apiTypes = () => {
@@ -214,55 +208,19 @@ const apiTypes = () => {
             createElem('div', document.body, null, `<p>Types</p>`, 'wrapper-types');
             wrapperTypes = document.querySelector('.wrapper-types');
             results.forEach(el => {
-                btnType = document.createElement('button');
-                btnType.innerHTML = el.name;
-                btnType.classList.add('btn');
-                wrapperTypes.append(btnType);
+                const btnType = createElem('button', wrapperTypes, null, el.name, 'btn');
                 btnType.addEventListener('click', () => {
                     fetch(el.url)
                         .then(res => res.json())
                         .then(res => {
                             const { pokemon } = res;
                             pokemons = pokemon;
-                            wrapper.innerHTML = '';
                             if (pokemons.length > 0) {
-                                pokemons.slice(0, pagination.limit-1).forEach(el => {
-                                    const { pokemon } = el;
-                                    const { name } = pokemon;
-                                    const pokemonClass = favourites.includes(name) ?
-                                        'selected-pokemon' :
-                                        'pokemon';
-
-                                    createElem('button', wrapper, null, name, pokemonClass, `${url}pokemon/${name}`);
-                                });
+                                paginatePokemons(0, pagination.limit-1, 'pokemon');
                             } else {
-                                wrapper.innerHTML = `<p>No pokemon of this type :(</p>`
+                                wrapper.innerHTML = `<p>No pokemon of this type :(</p>`;
                             }
-                            
-                            wrapperPaginat.innerHTML = '';
-                            const countPages = Math.floor(pokemons.length / pagination.limit);
-                            createPag(countPages);
-                            const btnPaginat = document.querySelectorAll('.btn-paginat');
-
-                            btnPaginat.forEach((elem, index) => {
-                                elem.setAttribute('data-id', index + 1);
-                                elem.setAttribute('href', '#');
-                                elem.addEventListener('click', function (e){
-                                    e.preventDefault();
-                                    wrapper.innerHTML = '';
-                                    const start = pagination.limit * (elem.getAttribute('data-id') - 1);
-                                    const end = pagination.limit * elem.getAttribute('data-id') - 1;
-                                    pokemons.slice(start, end).forEach(el => {
-                                        const { pokemon } = el;
-                                        const { name } = pokemon;
-                                        const pokemonClass = favourites.includes(name) ?
-                                            'selected-pokemon' :
-                                            'pokemon';
-
-                                        createElem('button', wrapper, null, name, pokemonClass, `${url}pokemon/${name}`);
-                                    });
-                                })
-                            })
+                            createPagination('pokemon');
                         })
                 })
             })
