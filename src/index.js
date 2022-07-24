f2cf5aa20034421e5b34e8b6f3cab90dec205e65
krupskayaa <alyona.krupskaya@gmail.com>
2022-07-24 17:50:01 +0300

    worked on hw-29

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import './styles/all'
+import './styles/all.scss';
+import moment from 'moment';
 import { MDCDataTable } from '@material/data-table';
+import students from "../students.json";
+import Chart from 'chart.js/auto';
 
-let root = document.getElementById("root");
+let wrapper = document.querySelector('.wrapper-all')
 
-let wrapper = document.createElement("div");
-wrapper.className = "wrapper";
-root.append(wrapper);
+const createElem = (tag, append, text = null, clName) => {
+  let el = document.createElement(tag);
+  if(text){
+    el.innerHTML = text;
+  }
+  el.classList.add(clName);
+  append.prepend(el);
+}
 
-wrapper.innerHTML = `
+createElem('div', wrapper, `
 <div class="mdc-data-table">
   <div class="mdc-data-table__table-container">
     <table class="mdc-data-table__table" aria-label="Students">
@@ -22,11 +30,38 @@ wrapper.innerHTML = `
     </table>
   </div>
 </div>
-`;
-const dataTable = new MDCDataTable(document.querySelector(".mdc-data-table"));
+`, 'wrapper');
 
+const dataTable = new MDCDataTable(document.querySelector('.mdc-data-table'));
+const { content } = dataTable;
 
+let names = students.map(el => el.name);
+let rating = students.map(el => el.rating);
 
-console.log(dataTable)
+students.forEach((elem) => {
+  let date = moment(`${elem.birthday}`, "DD-MM-YYYY").format('LL');
+  createElem('tr', content, `
+  <th class="mdc-data-table__cell" scope="row">${elem.name}</th>
+  <td class="mdc-data-table__cell mdc-data-table__cell--numeric">${date}</td>
+  <td class="mdc-data-table__cell mdc-data-table__cell--numeric">${elem.rating}</td>`, 'mdc-data-table__row')
+});
 
-// console.log('test')
+const ctx = document.getElementById('lineChart');
+
+new Chart(ctx, {
+  responsive: true,
+  maintainAspectRatio: false,
+  type: 'line',
+  options: {
+    maintainAspectRatio: false,
+  },
+  data: {
+    labels: names,
+    datasets: [{
+      label: 'Rating',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: rating,
+    }]
+  }
+})
