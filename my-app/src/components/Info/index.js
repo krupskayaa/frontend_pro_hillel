011ef4bdf7517e5bf75146c3d1f094c11aceb54d
krupskayaa <alyona.krupskaya@gmail.com>
2022-08-06 10:42:41 +0300

    worked on hw-30

--- my-app/src/components/Info/index.js
+++ my-app/src/components/Info/index.js
@@ -6,6 +6,10 @@ function Info(props) {
     return (
         <div className = {props.class}>
             <Photo class = "photo"></Photo>
+            <div className = "main-info">            
+                <h2>Krupska Alona</h2>
+                <p>I'm a Frontend Developer.</p>
+            </div>
         </div>
     );
 }
